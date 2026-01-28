f1f1b026d711d496a4ca348fa27c12036ebca783
UEFI-code <zhuoshizhang@hotmail.com>
2026-01-28 20:14:17 +0900

    Try new hyper-param

--- conf.py
+++ conf.py
@@ -1,8 +1,8 @@
 contextSize = 64
 embedding_dim = 4
-hidden_dim = 64
+hidden_dim = 128
 depth = 3
 
 batchSize = 64
-epoch = 1000
-learning_rate, weight_decay = 1e-4, 1e-5
+epoch = 3000
+learning_rate, weight_decay = 1e-3, 1e-5
